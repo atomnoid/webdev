07644420bbb85c1713d610d332b722e85eb53d0e
atomnoid <aayushs2580@gmail.com>
2025-04-03 01:13:40 +0530

    i learn variables today

---
+++ variables.js
@@ -0,0 +1,11 @@
+// The const keyword is used to declare a variable that cannot be reassigned
+const pi = 3.14;
+pi = 3.14159; // This will throw an error because pi is a constant
+
+// The let keyword is used to declare a variable that can be reassigned
+let radius = 5;
+radius = 10; // This is allowed because radius is declared with let
+
+// The var keyword is used to declare a variable that can be reassigned and has function scope
+var area = pi * radius * radius;
+console.log(area); // Output: 314.0
